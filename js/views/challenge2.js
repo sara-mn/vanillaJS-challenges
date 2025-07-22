3e0e4855752115785447dda8b38a25a73668a853
Sara <smohaqeqn@gmail.com>
2025-07-22 17:08:36 +0330

    base structure edited

--- js/views/challenge2.js
+++
@@ -1,9 +0,0 @@
-export default function challenge2() {
-  const div = document.createElement('div');
-  div.innerHTML = `
-    <h2>Challenge 2</h2>
-    <p>This is the second challenge.</p>
-    <a href="#/">Back</a>
-  `;
-  return div;
-}
