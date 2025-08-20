4db0c99d4335a58e807075d0bd4329b7b020c50c
Sara <smohaqeqn@gmail.com>
2025-08-20 14:48:04 +0330

    **//ref//** webpack setting and some directories refactored

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -8,13 +9,23 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    clean: true,
+    // clean: true,
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
     new CopyPlugin({
       patterns: [
         {from: 'src/views', to: 'views'},
-        {from: 'index.html', to: 'index.html'},
+        {from: 'favicon.ico', to: 'favicon.ico'},
+        {from: '404.html', to: '404.html'},
+        { from: 'src/assets/img', to: 'img' },
+        { from: 'src/assets/css', to: 'css' },
+        { from: 'src/assets/icon.svg', to: 'icon.svg' },
+        { from: 'src/assets/robots.txt', to: 'robots.txt' },
+        { from: 'src/assets/icon.png', to: 'icon.png' },
+        { from: 'src/assets/site.webmanifest', to: 'site.webmanifest' },
       ],
     }),
   ],
