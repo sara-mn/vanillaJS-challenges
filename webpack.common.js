62c19ea90045595f24a1be0a0e80ed06abbc1df2
Sara <smohaqeqn@gmail.com>
2025-08-20 15:29:04 +0330

    **//ref//** package.json and webpack refactored

--- webpack.common.js
+++ webpack.common.js
@@ -1,31 +1,37 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
-    app: './src/index.js',
+    app: './src/app.js',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'bundle.[contenthash].js',
     // clean: true,
   },
   plugins: [
+    new CleanWebpackPlugin(), // dist رو قبل از build پاک می‌کنه
     new HtmlWebpackPlugin({
       template: './src/index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+      },
     }),
     new CopyPlugin({
       patterns: [
         {from: 'src/views', to: 'views'},
         {from: 'favicon.ico', to: 'favicon.ico'},
         {from: '404.html', to: '404.html'},
-        { from: 'src/assets/img', to: 'img' },
-        { from: 'src/assets/css', to: 'css' },
-        { from: 'src/assets/icon.svg', to: 'icon.svg' },
-        { from: 'src/assets/robots.txt', to: 'robots.txt' },
-        { from: 'src/assets/icon.png', to: 'icon.png' },
-        { from: 'src/assets/site.webmanifest', to: 'site.webmanifest' },
+        {from: 'src/assets/img', to: 'img'},
+        {from: 'src/assets/css', to: 'css'},
+        {from: 'src/assets/icon.svg', to: 'icon.svg'},
+        {from: 'src/assets/robots.txt', to: 'robots.txt'},
+        {from: 'src/assets/icon.png', to: 'icon.png'},
+        {from: 'src/assets/site.webmanifest', to: 'site.webmanifest'},
       ],
     }),
   ],
@@ -35,9 +41,17 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource', // برای تصاویر
+      },
     ],
   },
   devServer: {
+    static: './dist',
+    historyApiFallback: true, // اگر Routing داری
+    open: true,
+    hot: true,
     static: './dist',
     historyApiFallback: true,
   },
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,8 +1,15 @@
-const { merge } = require('webpack-merge');
+const {merge} = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
-  plugins: [
-  ],
+  output: {
+    publicPath: '', // برای GitHub Pages مهمه
+  },
+  devServer: {
+    static: './dist',
+    historyApiFallback: true, // اگر Routing داری
+    open: true,
+    hot: true,
+  },
 });
