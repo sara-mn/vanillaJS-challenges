9349d0308fc605ab28e6465a9bba3e3aa1c60c5c
Sara <smohaqeqn@gmail.com>
2025-08-25 15:17:21 +0330

    **//ref//** scripts changes

--- src/views/challenge1/script.js
+++ src/views/challenge1/script.js
@@ -1,48 +1,60 @@
-document.querySelectorAll(".qube").forEach((btn) => {
-  btn.addEventListener("click", () => {
-
-    if (window.currentSong) {
-      window.currentSong.pause();
-      window.currentSong.currentTime = 0;
-    }
-
-    const song = btn.querySelector("audio");
-    if (song) {
-      song.play().then();
-      window.currentSong = song;
-    }
-  });
-
-  btn.addEventListener("mousedown", (event) => {
-    btn.firstElementChild.classList.add("focused");
-  });
-
-  btn.addEventListener("mouseup", (event) => {
-    btn.firstElementChild.classList.remove("focused");
-  });
-
-  btn.addEventListener("mouseleave", (event) => {
-    btn.firstElementChild.classList.remove("focused");
-  });
-
-});
-
-let playBtn;
-
-document.addEventListener("keydown", (event) => {
-  playBtn = document.getElementById(event.key);
-  if (playBtn) {
-    playBtn.classList.add("focused");
+window.addEventListener("hashchange", () => {
+  if (location.hash === "#/challenge/1/drum-kit") {
+    setTimeout(drumKit, 50);
   }
 });
 
-document.addEventListener("keyup", (event) => {
-  playBtn = document.getElementById(event.key);
-  if (playBtn) {
-    playBtn.click();
-    playBtn.classList.remove("focused");
-  }
-});
+drumKit();
+
+function drumKit() {
+  document.querySelectorAll(".qube").forEach((btn) => {
+    btn.addEventListener("click", () => {
+
+      if (window.currentSong) {
+        window.currentSong.pause();
+        window.currentSong.currentTime = 0;
+      }
+
+      const song = btn.querySelector("audio");
+      if (song) {
+        song.play().then();
+        window.currentSong = song;
+      }
+    });
+
+    btn.addEventListener("mousedown", (event) => {
+      btn.firstElementChild.classList.add("focused");
+    });
+
+    btn.addEventListener("mouseup", (event) => {
+      btn.firstElementChild.classList.remove("focused");
+    });
+
+    btn.addEventListener("mouseleave", (event) => {
+      btn.firstElementChild.classList.remove("focused");
+    });
+
+  });
+
+  let playBtn;
+
+  document.addEventListener("keydown", (event) => {
+    playBtn = document.getElementById(event.key);
+    if (playBtn) {
+      playBtn.classList.add("focused");
+    }
+  });
+
+  document.addEventListener("keyup", (event) => {
+    playBtn = document.getElementById(event.key);
+    if (playBtn) {
+      playBtn.click();
+      playBtn.classList.remove("focused");
+    }
+  });
+}
+
+
 
 
 
--- src/views/challenge3/script.js
+++ src/views/challenge3/script.js
@@ -1,23 +1,35 @@
-const spacingElement = document.getElementById("spacing");
-const blurElement = document.getElementById("blur");
-const colorElement = document.getElementById("colorInput");
-
-
-spacingElement.addEventListener("input", (event) => {
- console.log(event.target.value);
-  const mainElement = document.getElementsByClassName("main-bg")[0];
-  mainElement.style.backgroundPositionX = 50 + (event.target.valueAsNumber / 2) + "%";
-  mainElement.style.backgroundPositionY = 50 + (event.target.valueAsNumber / 2) + "%";
+window.addEventListener("hashchange", () => {
+  if (location.hash === "#/challenge/3/playing-with-CSS-Variables") {
+    setTimeout(play, 50);
+  }
 });
 
-blurElement.addEventListener("input", (event) => {
- console.log(event.target.value);
-  const mainElement = document.getElementsByClassName("play")[0];
-  mainElement.style.backdropFilter = `blur(${event.target.value}px)`
-});
+play();
 
-colorElement.addEventListener("input", (event) => {
- console.log(event.target.value);
-  const mainElement = document.getElementsByClassName("main-bg")[0];
-  mainElement.style.backgroundColor = `${event.target.value}`
-});
+function play() {
+  const spacingElement = document.getElementById("spacing");
+  const blurElement = document.getElementById("blur");
+  const colorElement = document.getElementById("colorInput");
+
+
+  spacingElement.addEventListener("input", (event) => {
+    console.log(event.target.value);
+    const mainElement = document.getElementsByClassName("main-bg")[0];
+    mainElement.style.backgroundPositionX = 50 + (event.target.valueAsNumber / 2) + "%";
+    mainElement.style.backgroundPositionY = 50 + (event.target.valueAsNumber / 2) + "%";
+  });
+
+  blurElement.addEventListener("input", (event) => {
+    console.log(event.target.value);
+    const mainElement = document.getElementsByClassName("play")[0];
+    mainElement.style.backdropFilter = `blur(${event.target.value}px)`
+  });
+
+  colorElement.addEventListener("input", (event) => {
+    console.log(event.target.value);
+    const mainElement = document.getElementsByClassName("main-bg")[0];
+    mainElement.style.backgroundColor = `${event.target.value}`
+  });
+
+}
+
