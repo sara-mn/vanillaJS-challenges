d7acf6df6f964a7619592f8d20e524b814a1013f
Sara <smohaqeqn@gmail.com>
2025-09-07 18:45:59 +0330

    **//feat//** challenge4 6 created

---
+++ src/views/challenge6/script.js
@@ -0,0 +1,33 @@
+window.addEventListener("hashchange", () => {
+  if (location.hash === "#/challenge/6/ajax_type_ahead") {
+    setTimeout(play, 50);
+  }
+});
+
+play();
+
+function play() {
+  const items = document.querySelectorAll(".item");
+  items.forEach(panel => {
+    panel.addEventListener("click", (e) => {
+      panel.classList.toggle("open");
+    })
+
+  });
+  items.forEach(panel => {
+    panel.addEventListener("transitionend", (e) => {
+      if (e.propertyName.includes("flex")) {
+        const flexGrowValue = getComputedStyle(panel).flexGrow;
+        if (flexGrowValue === "4") {
+          panel.classList.add("active-panel");
+        }
+        else
+          panel.classList.remove("active-panel");
+
+      }
+    })
+
+  });
+
+}
+
