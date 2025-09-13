8a762eab55af095c458058934eca1ccfae4d1d07
Sara <smohaqeqn@gmail.com>
2025-09-13 19:27:43 +0330

    **//feat//** challenge6 search function written

--- src/views/challenge6/script.js
+++ src/views/challenge6/script.js
@@ -1,33 +1,47 @@
 window.addEventListener("hashchange", () => {
   if (location.hash === "#/challenge/6/ajax_type_ahead") {
-    setTimeout(play, 50);
+    setTimeout(fetchData, 50);
   }
 });
 
-play();
+await fetchData();
 
-function play() {
-  const items = document.querySelectorAll(".item");
-  items.forEach(panel => {
-    panel.addEventListener("click", (e) => {
-      panel.classList.toggle("open");
-    })
+async function fetchData() {
+  const url = "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+  let resultEl = document.getElementById("result");
+  const cities = await getDataApi(url);
 
-  });
-  items.forEach(panel => {
-    panel.addEventListener("transitionend", (e) => {
-      if (e.propertyName.includes("flex")) {
-        const flexGrowValue = getComputedStyle(panel).flexGrow;
-        if (flexGrowValue === "4") {
-          panel.classList.add("active-panel");
-        }
-        else
-          panel.classList.remove("active-panel");
-
+  document.getElementById('searchBtn').addEventListener('keyup', (e) => {
+    resultEl.innerHTML = '';
+    const str = e.target.value;
+    if (str.length > 2) {
+      const filteredList = findMatches(cities, str);
+      if (filteredList.length > 0)
+        filteredList.forEach(c => {
+          const li = document.createElement("li");
+          li.innerText = c['city']?.toString()
+          resultEl.appendChild(li);
+        })
+      else {
+        const li = document.createElement("li");
+        li.innerText = 'Not Found!'
+        resultEl.appendChild(li);
       }
-    })
-
-  });
-
+    }
+  })
 }
 
+function findMatches(cities, key) {
+  const str = `/${key}/i`
+  const regex = new RegExp(key, 'i');
+  return cities.filter(item => item['city']?.match(regex));
+}
+
+async function getDataApi(url, key) {
+  const response = await fetch(url, {method: "GET"});
+  try {
+    return await response.json();
+  } catch (e) {
+    console.log(e);
+  }
+}
